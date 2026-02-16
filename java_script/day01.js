a8b75c0e6b14c993d468b8ed5d5ae710678c2eb6
omkumar122122122 <omkumar122122122@gmail.com>
2026-02-16 14:10:36 +0530

    methords of array

--- java_script/day01.js
+++ java_script/day01.js
@@ -1,3 +1,77 @@
-let arr = [2,3];
-arr.unshift(1);
-console.log(arr); // [1,2,3]
+//learnning js in built function.
+// unshift()
+// let arr = [4,5];
+// arr.unshift(1,2,4);
+// console.log(arr); // [1,2,3]
+//map()
+// let arr = [1,2,3];
+// let result = arr.map(x => 2 ** x);
+// console.log(result); 
+//filter()
+// let arr = [1,2,3,4];
+// let result = arr.filter(x => x%2==0);
+// console.log(result); 
+//reduce
+// let arr = [1,2,3];
+// let sum = arr.reduce((acc,x) => acc + x, 0);
+// console.log(sum); // 6
+// let nums = [1, 2, 3, 4, 5, 6];
+
+// let sumEven = nums.reduce((acc, curr) => {
+//     if (curr % 2 === 0) {
+//         acc += curr;
+//     } else {
+//         acc += 0;
+//     }
+//     return acc;
+// }, 0);
+
+// console.log(sumEven); // 12
+//forEach()
+// let arr = [1,2,3];
+// let arr2=[];
+// arr.forEach(x =>arr2.push(x));
+// console.log(arr2)
+//find
+// let arr = [1,2,3];
+// let result = arr.find(x => x > 1);
+// console.log(result); // 2
+// let arr = [1,2,3];
+// let result = arr.find(x => x > 4);
+// if(result==undefined){
+//     console.log('not found')
+// }
+// else{
+//     console.log(result);
+
+// }
+//find index
+// let arr = [1,2,3];
+// let index = arr.findIndex(x => x > 4);
+// console.log(index); // 1
+//returns -1 if not found
+//includes()
+// let arr = [1,2,3];
+// console.log(arr.includes(2)); // true
+// sclice
+// let arr = [0,1,2,3,4];
+// console.log(arr.slice(1,3)); 
+//splice()
+// let arr = [0,1,2,3,4,5,6,7,8];
+// arr.splice(2,0,9);
+// console.log(arr); // [1,3]
+
+
+
+
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+
+
+
+
+
+
+
