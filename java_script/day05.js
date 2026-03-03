ddea9e583906b939a0d9729ab24c0f4f118f09a1
omkumar122122122 <omkumar122122122@gmail.com>
2026-03-03 12:14:59 +0530

    after long time

--- java_script/day05.js
+++ java_script/day05.js
@@ -15,22 +15,52 @@
 // Select an element with id "demo" and add a border to it using JavaScript.
 // Select an input field with id "username" and print its value in the console.
 // Select a button with id "btn" and disable it.
-let demo=document.getElementById('demo')
-let i=document.getElementById('alpha')
-let j=document.querySelectorAll('li')
-j[0].style.color='red';
-i.innerHTML='nope'
-demo.style.border='2px,solid,black'
-demo.style.padding='2px'
-demo.innerText='hom';
-demo.style.color='blue';
-demo.style.backgroundColor='yellow'
-i.style.color='red'
-j[0].style.backgroundColor='yellow';
-let c=document.getElementById('name')
-let char="om kumar"
-if(char=="om kumar"){
-    demo.style.color='red'
-}
-
-
+// let demo=document.getElementById('demo')
+// let i=document.getElementById('alpha')
+// let j=document.querySelectorAll('li')
+// j[0].style.color='red';
+// i.innerHTML='nope'
+// demo.style.border='2px,solid,black'
+// demo.style.padding='2px'
+// demo.innerText='hom';
+// demo.style.color='blue';
+// demo.style.backgroundColor='yellow'
+// i.style.color='red'
+// j[0].style.backgroundColor='yellow';
+// let c=document.getElementById('name')
+// let char="om kumar"
+// if(char=="om kumar"){
+//     demo.style.color='red'
+// }
+let demo=document.querySelector('#demo')
+demo.style.color='red';
+let cou=document.querySelector('#cout')
+let name=document.querySelector('#name')
+document.querySelector('#on').addEventListener("click",()=>{
+    let count=0;
+    for(i in name.value){
+        count++
+    }
+    console.log(count);
+    cou.innerHTML=`hello ${name.value}`;
+    if(name.value=="om kumar"){
+        demo.style.color='blue';
+    }
+    else{
+        demo.style.color='red';  
+    }
+})
+document.querySelector('#submit').addEventListener("click",()=>{
+    let count=0;
+    for(i in name.value){
+        count++
+    }
+    console.log(count);
+    cou.innerHTML=`hello ${name.value}`;
+    if(name.value=="om kumar"){
+        demo.style.color='blue';
+    }
+    else{
+        demo.style.color='red';  
+    }
+})
---
+++ java_script/day06DOM.js
@@ -0,0 +1,8 @@
+// //learning DOM
+// // const text = document.getElementById("demo");
+// const text = document.querySelector("p.faaa")
+// // text.innerHTML = "Hello World!"
+// text.style.border='2px solid black'
+// // text.style.
+// document.createElement("p").innerHTML="hii"
+// document.body.appendChild(p)
