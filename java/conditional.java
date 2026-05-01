381421e1a206592a0134af44d4181ba52e6949ef
omkumar122122122 <omkumar122122122@gmail.com>
2026-05-01 15:38:18 +0530

    conditional statement

--- java/conditional.java
+++ java/conditional.java
@@ -1,3 +1,17 @@
+import java.util.*;
+
 public class conditional {
+    public static void main(String[] srgs){
+        Scanner sc=new Scanner(System.in);
+        int age=sc.nextInt();
+        if(age<0){
+            System.out.print("invalid age ");
+        }
+        if (age<18) {
+            System.out.print("not adult");
+        } else {
+            System.out.print("adult you are eligibal for vote");
+        }
+    }
     
 }
