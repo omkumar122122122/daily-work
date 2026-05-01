7c2796515866a3740f891a5a2c0f2ed209ddeb13
omkumar122122122 <omkumar122122122@gmail.com>
2026-05-01 16:11:28 +0530

    loops in java

---
+++ java/loops.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class loops {
+    public static void main(String[] argus){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter no. whouse table is to be printd");
+        int a;
+        a=sc.nextInt();
+        for(int i=1;i<=10;i++){
+            System.out.println(a+"*"+i+"="+a*i);
+        }
+    }
+    
+}
