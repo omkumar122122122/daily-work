85cadab52a6ec08315514e62a1afd96b4a8055a2
omkumar122122122 <omkumar122122122@gmail.com>
2026-05-02 18:21:32 +0530

    pattrn

---
+++ java/tilt_rec.java
@@ -0,0 +1,15 @@
+public class tilt_rec {
+   public static void main(String[] args){
+    int a=5,b=5;
+    for(int i=1;i<=a;i++){
+        for(int j=1;j<=b-i;j++){
+            System.out.print("  ");
+        }
+        for(int j=1;j<=b;j++){
+            System.out.print("* ");
+        }
+        
+        System.out.print("\n ");
+    }
+   }
+}
---
+++ java/tringle.java
@@ -0,0 +1,30 @@
+public class tringle {
+    public static void main(String[] args){
+        int a=4,b=4;
+        for(int i=1;i<=4;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print("* ");
+            }
+            for(int j=1;j<=(2*(a-i));j++){
+                System.out.print("  ");
+            }
+            for(int j=1;j<=i;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+            
+        }
+        for(int i=a;i>=1;i--){
+            for(int j=1;j<=i;j++){
+                System.out.print("* ");
+            }
+            for(int j=1;j<=(2*(a-i));j++){
+                System.out.print("  ");
+            }
+            for(int j=1;j<=i;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
