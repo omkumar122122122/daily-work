4f0d7802e1ee67018b34d5ff84097a6c8bf7c27a
omkumar122122122 <omkumar122122122@gmail.com>
2026-05-06 12:43:36 +0530

    input done

--- java/array03.java
+++ java/array03.java
@@ -7,6 +7,7 @@ public class array03 {
         int[][] arr=new int[row][col];
         System.out.println("enter elements of array");
         for(int i=0;i<row;i++){
+            System.out.println("enter element of row"+i+1+"\n");
             for(int j=0;j<col;j++){
                 arr[i][j]=sc.nextInt();
             }
