38679458d5ded02ed3e012fbc9bfb37dfc4e4bae
omkumar122122122 <omkumar122122122@gmail.com>
2026-05-06 12:46:33 +0530

    output done

--- java/array03.java
+++ java/array03.java
@@ -13,10 +13,10 @@ public class array03 {
             }
         }
         for(int i=0;i<row;i++){
-            System.out.println("enter element of row"+(i+1)+"\n");
             for(int j=0;j<col;j++){
-                arr[i][j]=sc.nextInt();
+                System.out.print(arr[i][j]);
             }
+            System.out.println("\n");
         }
 
 
