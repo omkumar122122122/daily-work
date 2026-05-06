6562f1864c98687c5a0606f91ddb42c9d89f24d6
omkumar122122122 <omkumar122122122@gmail.com>
2026-05-06 12:37:24 +0530

    2 d array input

--- java/array03.java
+++ java/array03.java
@@ -1,6 +1,13 @@
+import java.util.*;
 public class array03 {
     public static void main(String[] args){
-        
+        Scanner sc=new Scanner(System.in);
+        int row=4;
+        int col=4;
+        for()
+
+
+       
         
     }
 }
