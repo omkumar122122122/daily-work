91a28d423c21e26c32b74d5c990e5fb759c81a31
omkumar122122122 <omkumar122122122@gmail.com>
2026-05-06 12:38:36 +0530

    col

--- java/array03.java
+++ java/array03.java
@@ -5,7 +5,9 @@ public class array03 {
         int row=4;
         int col=4;
         for(int i=0;i<row;i++){
-            
+            for(int j=0;j<col;j++){
+                
+            }
         }
 
 
