bad63e622ac524a4fe6a570802c33eb70f0ce900
omkumar122122122 <omkumar122122122@gmail.com>
2026-05-06 12:19:40 +0530

    array

--- java/array02.java
+++ java/array02.java
@@ -3,6 +3,11 @@ public class array02 {
     public static void main(String[] args){
       int arr[]=new int[10];
       Scanner sc=new Scanner(System.in);
-        
+      for(int i=0;i<10;i++){
+        arr[i]=sc.nextInt();
+      }
+      System.out.println("enter nuber to search");
+      
+
     }
 }
