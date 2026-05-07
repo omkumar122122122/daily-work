4457b1ac0d735d4bbc88a3276752e1a07cb5e81f
omkumar122122122 <omkumar122122122@gmail.com>
2026-05-07 12:54:41 +0530

    input

--- java/array04.java
+++ java/array04.java
@@ -1,14 +1,17 @@
 import java.util.*;
 public class array04 {
     public static void main(String[] args){
+
         Scanner sc =new Scanner(System.in);
         System.out.println("enter elementes of the matrix");
         int row=4;
         int col=4;
+        int[][] arr=new int[row][col];
         for(int i=0;i<row;i++){
             System.out.println("enter the elements of row "+(i+1));
             for(int j=0;j<col;j++){
-                
+                arr[i][j]=sc.nextInt();
+
             }
 
         }
