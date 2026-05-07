4623aec5518a4599bb15aedda8dbfdf657e7e70d
omkumar122122122 <omkumar122122122@gmail.com>
2026-05-07 12:49:43 +0530

    message

--- java/array04.java
+++ java/array04.java
@@ -2,6 +2,7 @@ import java.util.*;
 public class array04 {
     public static void main(String[] args){
         Scanner sc =new Scanner(System.in);
+        System.out.println("enter elementes of the matrix");
         
 
         
