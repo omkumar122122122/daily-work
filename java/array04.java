939668849b61216805b85172fb57ca4a6e4b2f71
omkumar122122122 <omkumar122122122@gmail.com>
2026-05-08 12:58:07 +0530

    condition

--- java/array04.java
+++ java/array04.java
@@ -18,7 +18,9 @@ public class array04 {
         System.out.println("enter number to search");
         int n=sc.nextInt();
         for(int i=0;i<row;i++){
-            for(int j=0;j<col;j++)
+            for(int j=0;j<col;j++){
+                
+            }
 
         }
 
