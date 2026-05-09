5275ffc46a73494e1817ca1c1f8382a7e170be4a
omkumar122122122 <omkumar122122122@gmail.com>
2026-05-09 10:41:20 +0530

    understanding about the directory rule

---
+++ java/basic/array/array_function/shorting.java
@@ -0,0 +1,13 @@
+package basic.array.array_function;
+
+import java.util.Scanner;
+
+public class shorting {
+
+     public static void main(String[] var0) {
+      Scanner sc=new Scanner(System.in);
+      int[] var2 = new int[10];
+      System.out.println(var2);
+   }
+    
+}
