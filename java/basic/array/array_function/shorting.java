d0ad2eac64bc311c6e82a739fa790d1fe3524192
omkumar122122122 <omkumar122122122@gmail.com>
2026-05-09 11:41:35 +0530

    sort completed

--- java/basic/array/array_function/shorting.java
+++ java/basic/array/array_function/shorting.java
@@ -2,7 +2,6 @@
 
 // import java.util.Arrays;
 
-Function	Syntax	Short Description	Example
 // sort()	Arrays.sort(arr);	
 // Sorts the array in ascending order.	
 // int[] a={5,2,8}; Arrays.sort(a); // {2,5,8}
@@ -14,7 +13,8 @@ public class shorting {
 
      public static void main(String[] var0) {
       Scanner sc=new Scanner(System.in);
-      int[] var2 = {1,2,3};
+      int[] var2 = {1,2,3,8,9,5,6,4,9,3,4,7,5,4};
+      Arrays.sort(var2);
       System.out.println(Arrays.toString(var2));
    }
     
