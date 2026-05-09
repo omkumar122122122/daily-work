4c8c51ea46249053daba6b85c3205303b469a486
omkumar122122122 <omkumar122122122@gmail.com>
2026-05-09 12:31:23 +0530

    rev_string

--- java/basic/array/array_function/shorting.java
+++ java/basic/array/array_function/shorting.java
@@ -1,4 +1,4 @@
-// package basic.array.array_function;
+package basic.array.array_function;
 
 // import java.util.Arrays;
 
---
+++ java/basic/string/string_builder.java
@@ -0,0 +1,15 @@
+package basic.string;
+
+public class string_builder {
+    public static void main (String[] args){
+        StringBuilder sb=new StringBuilder("aman");
+        StringBuilder s2=new StringBuilder("aman");
+        for(int i=sb.length()-1;i>0;i++){
+
+            s2.append(sb.charAt(i));
+
+
+    }
+    }
+    
+}
