58ce3129f83ac0d7138b5f779c23b036ec4bbdf4
omkumar122122122 <omkumar122122122@gmail.com>
2026-05-11 18:37:58 +0530

    get bit

--- java/basic/string/BitManupualtion.java
+++ java/basic/string/BitManupualtion.java
@@ -1,4 +1,20 @@
+import java.util.*;
 public class BitManupualtion {
-    
+    public static void main(String[] args ){
+        Scanner sc=new Scanner(System.in);
+        int n=5;
+        System.out.println("enter position ");
+        int pos=sc.nextInt();
+        int bit_mask=n<<pos;
+        if((bit_mask&n)==0){
+            System.out.println("the bit is zero");
+
+        }
+        else{
+            System.out.println("the bit is one");
+        }
+        sc.close();
+
+    }
     
 }
