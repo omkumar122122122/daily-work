3daf8ecdd014ebfb8e88e15567dcfcbf53ee91a8
omkumar122122122 <omkumar122122122@gmail.com>
2026-05-12 12:44:08 +0530

    learning opp

---
+++ java/OOP/first_class.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+
+class pen{
+
+    String name;
+    String type;
+
+    public void write(){
+        System.out.println("hiiiiiooolll");
+        
+    }
+
+}
+
+public class first_class {
+    
+}
