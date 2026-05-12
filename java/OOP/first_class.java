9935adbb14014313f705986cdf08de66dcbee2a1
omkumar122122122 <omkumar122122122@gmail.com>
2026-05-12 12:49:16 +0530

    classes

--- java/OOP/first_class.java
+++ java/OOP/first_class.java
@@ -7,12 +7,21 @@ class pen{
     String type;
 
     public void write(){
-        System.out.println("hiiiiiooolll");
-        
+        System.out.println(this.name+this.type);
+
     }
 
 }
 
 public class first_class {
+   public static void main (String[] stgs){
+     pen pen1=new pen();
+    pen1.name="hhmm ";
+    pen1.type="gel";
+    pen1.write();
+
+
+    
+   }
     
 }
