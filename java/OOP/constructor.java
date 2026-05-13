f7ebc4fbd38ec141594f6d0ad67fef986e0c9490
omkumar122122122 <omkumar122122122@gmail.com>
2026-05-13 19:00:24 +0530

    constructor in java

---
+++ java/OOP/constructor.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+class Student{
+    String name;
+    String department;
+    String branch;
+
+    void detail(){
+        System.out.println(this.name+" \n"+this.branch);
+    }
+
+    Student(){
+        System.out.println("student added");
+    }
+    Student(String name ,String branch){
+        this.name=name;
+        this.branch=branch;
+
+    }
+   
+}
+
+public class constructor {
+
+    public static void main(){
+        Student s1= new Student();
+        Student s2=new Student("om kumar","cse");
+        s1.detail();
+        s2.detail();
+
+
+    }
+    
+}
