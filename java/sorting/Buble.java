3c2bea4c3bb179c9336110e63a6a27576f546a68
omkumar122122122 <omkumar122122122@gmail.com>
2026-05-15 10:51:46 +0530

    buble sort done

--- java/sorting/Buble.java
+++ java/sorting/Buble.java
@@ -3,7 +3,7 @@ import java .util.*;
 public class Buble {
     public static void BubleSort(int []arr){
         for(int i=arr.length;i>0;i--){
-            for(int j=0;j<i;j++){
+            for(int j=0;j<i-1;j++){
                 if(arr[j]>arr[j+1]){
                     int temp=arr[j];
                     arr[j]=arr[j+1];
@@ -13,9 +13,20 @@ public class Buble {
 
         }
     }
+    public static void PrintArr(int[] arr){
+        System.out.print("[");
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+        System.out.print("]");
+
+    }
 
     
     public static void main(String[] args){
+        int[]arr={1,2,8,12,8,7,6,4,2};
+        BubleSort(arr);
+        PrintArr(arr);
 
 
 
