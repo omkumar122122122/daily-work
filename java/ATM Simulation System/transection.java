96ebbdaf6d0ed2dc0a3a910b0b6dd66a4763dda7
omkumar122122122 <omkumar122122122@gmail.com>
2026-05-16 16:02:31 +0530

    adding history

--- java/ATM Simulation System/transection.java
+++ java/ATM Simulation System/transection.java
@@ -1,3 +1,16 @@
+import java.sql.Timestamp;
 public class transection {
+    String statement;
+    double amount;
+    Timestamp ts=new Timestamp(System.currentTimeMillis());
+    public void add_transection(String st,double am){
+        this.amount=am;
+        this.statement=st;
+
+    }
+
+    public void print(){
+        System.out.println(this.statement+" "+this.amount+" at "+this.ts);
+    }
     
 }
