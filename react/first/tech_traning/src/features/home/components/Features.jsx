8afc7e1336f55ebedceac65f98efadfdf3e76c88
omkumar122122122 <omkumar122122122@gmail.com>
2026-07-10 15:23:20 +0530

    learning making components

--- react/first/tech_traning/src/features/home/components/Features.jsx
+++ react/first/tech_traning/src/features/home/components/Features.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
 import Featrs_cards from './Featrs_cards'
+import image01 from 'C:\Users\OM KUMAR GUPTA\OneDrive\Desktop\daily work\react\first\tech_traning\src\assets\hero.png'
 
 function Features() {
   return (
     <div><div style={{ border: "1px solid black", padding: "24px" }}>
     <h2>Our Features</h2>
     <div style={{ display: "flex", gap: "16px" }}>
-        <Featrs_cards hed="Fast" decp="Everything loads quickly."/>
-        <Featrs_cards hed="Simple" decp="Easy to use for everyone."/>
-        <Featrs_cards hed="Reliable" decp="Everything loads quicklyWorks the same, every time."/>
+        <Featrs_cards hed="Fast" decp="Everything loads quickly." im={image01}/>
+        <Featrs_cards hed="Simple" decp="Easy to use for everyone." im={image01}/>
+        <Featrs_cards hed="Reliable" decp="Everything loads quicklyWorks the same, every time." im={image01}/>
+
     </div>
     </div></div>
   )
