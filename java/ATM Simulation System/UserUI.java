ee35ca97648344a8b13e4dc6d919955996387b04
omkumar122122122 <omkumar122122122@gmail.com>
2026-07-12 14:00:16 +0530

    making creating account part

--- java/ATM Simulation System/UserUI.java
+++ java/ATM Simulation System/UserUI.java
@@ -5,8 +5,26 @@ public class UserUI {
         Scanner sc=new Scanner (System.in);
 
         System.out.println("heloo....");
-        System.out.println("enter your option");
+        ArrayList<Bank> accounts=new ArrayList<>();
         
+
+        while(true){
+        System.out.println("enter your option");
+        System.out.println("1. create account");
+        System.out.println("2. login ");
+        int option=sc.nextInt();
+            if(option==1){
+                System.out.println("creating new account");
+                System.out.println("enter your pasword");
+                String pass=sc.next();
+                Bank Acc=new Bank(pass);
+                accounts.add(Acc);
+                System.out.println("your account number is "+Acc.acc);
+
+
+            }
+        }
+
 
 
        
