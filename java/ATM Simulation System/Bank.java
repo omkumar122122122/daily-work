998f85c5228d1a8869127262bc58c51dfbf9b447
omkumar122122122 <omkumar122122122@gmail.com>
2026-07-12 13:50:36 +0530

    making console base project

---
+++ java/ATM Simulation System/Bank.java
@@ -0,0 +1,18 @@
+
+public class Bank {
+    static int next_accountno=1000;
+   
+    
+    int acc;
+    String pass;
+    Bank(String pass){
+
+        this.acc=next_accountno++;
+        this.pass=pass;
+
+    }
+    
+    
+    } 
+    
+
---
+++ java/ATM Simulation System/UserUI.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class UserUI {
+    public static void main(String[] args){
+        Scanner sc=new Scanner (System.in);
+
+        System.out.println("heloo....");
+        System.out.println("enter your option");
+        
+
+
+       
+
+
+            
+        }
+    }
+    
+
