1b73871c794b484b000f7f434cfa726cc85cfc6b
omkumar122122122 <omkumar122122122@gmail.com>
2026-08-10 16:15:15 +0530

    auto-commit

--- node js/index.js
+++ node js/index.js
@@ -3,6 +3,40 @@ import express from "express";
 const app=express();
 const PORT =3000;
 
-app.get("/", (req, res) => { res.send("WHY ROHIT"); });
+app.get("/users", (req,res)=>{
+    res.status(200).json(users)
+})
+const users = [
+    {id:1, name:"Rohit"},
+    {id:2, name:"Chandu"},
+    {id:3, name: "Arya"}
+];
+
+// post
+app.post('/users', (req,res)=>{
+    const newUser = {
+        id:users.length+1,
+        name:req.body.name
+    }
+    users.push(newUser)
+    res.status(201).json(users)
+})
+
+// put
+
+app.put('/user/:id', (req,res)=>{
+    const id = Number(req.params.id);
+    const user = users.find(u => u.id===id)
+
+    if(!user) return res.status(404).json ({message:"User not found"});
+    user.name = req.body.name;
+    res.status(200).json(user);
+})
+app.delete('/users/:ids',(req,res)=>{
+    const id = Number(req.params.ids);
+    users = users.filter(u=> u.id!==id);
+    res.status(200).json(user)
+    
+})
 app.listen(PORT, () => {
     console.log(` OM IS Server IS NOT listening on WHY? PUCHTA HAI BHARAT http://localhost:${PORT}`)});
